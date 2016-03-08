9e3a753ca61e1816c0ede71deb659e4deb37285b
Olga Petrova <Olga@Olgas-MacBook-Pro-2.local>
2016-03-08 14:53:21 +0100

    Remove Ext JS files

--- build/temp/development/Admin/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /Users/Olga/Documents/ExtJS Sample Projects/SenchaTest/SenchaTestDemo/AdminDashboard/app.js
--- build/temp/development/AdminDashboard/sass/config.rb
+++
@@ -1,2 +0,0 @@
-Compass.add_project_configuration('../../../../../sass/config.rb')
-cache_path = '/Users/Olga/Documents/ExtJS Sample Projects/SenchaTest/SenchaTestDemo/AdminDashboard/build/.sass-cache'
